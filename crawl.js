24db967b92142eda2492ba7587a6db38bf5e670f
ttokttokttok <joshua.yjn.chung@gmail.com>
2023-12-02 20:42:24 -0800

    geturlsfrom html function

--- crawl.js
+++ crawl.js
@@ -1,3 +1,28 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody)
+  const linkElements = dom.window.document.querySelectorAll('a')
+  const urls = [...linkElements].map(v=>{
+    if (v.href.slice(0, 1) === '/') {
+      try {
+        const urlObj = new URL(`${baseURL}${v.href}`)
+        return urlObj.href
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`)
+      }      
+    } else {
+      try {
+        const urlObj = new URL(v.href)
+        return urlObj.href
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`)
+      }  
+    }
+    })
+  return urls
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString)
   const hostPath= `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +33,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
