30fcaaff96b35776dbb42306e137f652468ccc09
ttokttokttok <joshua.yjn.chung@gmail.com>
2023-12-02 22:08:12 -0800

    end of tutorial

---
+++ report.test.js
@@ -0,0 +1,13 @@
+const {sortPages} = require('./report.js')
+const {test, expect} = require('@jest/globals')
+
+test('sortPages 2 Pages', ()=>{
+  const input = {
+    'https://wagslane.dev/path': 1,
+    'https://wagslane.dev': 3
+  }
+
+  const actual = sortPages(input)
+  const expected = [['https://wagslane.dev', 3], ['https://wagslane.dev/path', 1]]
+  expect(actual).toEqual(expected)
+})
